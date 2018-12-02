f9be478a01b764955ff239040ac739dcf980e5fa
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2018-12-02 22:30:51 +0100

    rany

--- Game/Laws of the Wilderness/Assets/Scripts/HealthController.cs
+++ Game/Laws of the Wilderness/Assets/Scripts/HealthController.cs
@@ -8,27 +8,37 @@ public class HealthController : MonoBehaviour
     public bool CanBeWounded;
     public int MaxHealth;
     public int CurrentHealth;
+    public UnityEngine.UI.Text HPValueText;
 
     public DeathController DeathController;
+    public GameObject BigWound;
+    public GameObject SmallWound;
 
+    public Transform[] BigWoundsSpawnBox;
+    public Transform[] SmallWoundsSpawnBox;
+
+    System.Random random;
+    List<GameObject> Wounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateHpValue();
+        random = new System.Random();
+        Wounds = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Damage(int damage, Collision2D collision = null)
     {
         if (CanBeWounded)
         {
-            AddWound();
+            AddWound(damage > 1, collision);
         }
 
         if (CurrentHealth - damage <= 0)
@@ -41,11 +51,57 @@ public class HealthController : MonoBehaviour
         else
             CurrentHealth -= damage;
 
+        UpdateHpValue();
     }
 
-    void AddWound()
+    void AddWound(bool bigWound, Collision2D collision = null)
     {
+        Debug.Log("Add wound");
+        if (BigWound == null || SmallWound == null)
+        {
+            Debug.LogError("Wound prefabs not set");
+            return;
+        }
+        if (collision != null)
+        {
+            foreach (var cp in collision.contacts)
+            {
+                //TODO:Spawn in random wound spawner
 
+                GameObject wound;
+                Transform parentObject = null;
+                GameObject woundPrefab = null;
+                RectTransform rect;
+
+                if (bigWound)
+                {
+                    //Instantiate(bigWound ? BigWound : SmallWound, cp.point, Quaternion.identity);
+
+                    rect = BigWoundsSpawnBox[0].GetComponent<RectTransform>();
+                    woundPrefab = BigWound;
+                }
+                else
+                {
+                    rect = SmallWoundsSpawnBox[0].GetComponent<RectTransform>();
+                    woundPrefab = SmallWound;
+                }
+
+                parentObject = rect.gameObject.transform;
+                wound = Instantiate(woundPrefab, cp.point, Quaternion.identity);
+                wound.transform.SetParent(parentObject,false);
+                wound.transform.localPosition = new Vector3(Random.Range(0, rect.rect.width), Random.Range(0, rect.rect.height), 0);
+            }
+        }
+        else
+        {
+            Debug.LogError("Collision missing");
+        }
+    }
+
+    void UpdateHpValue()
+    {
+        if (HPValueText != null)
+            HPValueText.text = $"{CurrentHealth}/{MaxHealth}";
     }
 
 
