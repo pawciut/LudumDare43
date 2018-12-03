0da7a42bb83136eced44192d63e0ed5022f72553
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2018-12-04 00:18:42 +0100

    menu

---
+++ Game/Laws of the Wilderness/Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScreen : MonoBehaviour
+{
+
+    public Canvas MenuCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ShowMenu()
+    {
+        MenuCanvas.enabled = true;
+        MenuCanvas.gameObject.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Prologue");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
